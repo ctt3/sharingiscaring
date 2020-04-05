793dacff36d60a652ddb61cbc5bf507a585b4a61
Charlie Thiry <charlesthiry94@gmail.com>
2020-04-04 22:37:25 -0400

    all the pages link together

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     root 'static_pages#index'
 
     devise_for :users, path_names: { sign_in: :login, sign_out: :logout }, controllers: {
-		sessions: 'users/sessions'
+		sessions: 'users/sessions',
+		passwords: 'users/passwords',
+		registrations: 'users/registrations'
 	}
 
 
